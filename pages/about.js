056d627ca9e6db06337cf060bd8557ecae7c0b22
Annu Poddar <annupoddar444@gmail.com>
2022-08-19 08:12:38 +0530

    Changes in aboutus

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,14 @@
 import developer from '../config/developer'
-import {TextYellow, TextWhite, TextGreen, TextParagraph} from '../components/Text/Text'
 import {Button} from '../components/Button/Button'
-import {text_wrapper} from '../components/Text/Text.module.css'
-
+import styles from './about.module.css'
 function About () {
     return (
         <>
-            <div className={text_wrapper}>
-                <TextYellow>Hello,</TextYellow>
-                <TextWhite>I'm {developer.name}.</TextWhite>
-                <TextGreen>Web Developer</TextGreen>
-                <TextParagraph>To work in an environment where I can utilize my skills and knowledge to make most unique products with any technologies.</TextParagraph>
+            <div className={styles.text_wrapper}>
+                <h1>Hello,</h1>
+                <h2>I'm {developer.name}.</h2>
+                <h1 className={styles['text-green']}>Web Developer</h1>
+                <p>To work in an environment where I can utilize my skills and knowledge to make most unique products with any technologies.</p>
                 <Button text="Hire Me"/>
             </div>
         </>
--- widgets/Navbar/Navbar.js
+++ widgets/Navbar/Navbar.js
@@ -1,19 +1,18 @@
-import style from './navbar.module.css'
+import styles from './navbar.module.css'
 import { Button } from '../../components/Button/Button'
 function Navbar() {
     const items = ['Home', 'Skills', 'Projects', 'Blogs', 'Contat us']
     return (
         <>
-        <div className={style.nav_bg}>
-            <div>
-                <img src = "https://cdn-icons-png.flaticon.com/128/8099/8099491.png" height={50} width={50}/>
+        <div className={styles.nav_bg}>
+            <div className={styles.logo}>
+                    <img src = "https://cdn-icons-png.flaticon.com/128/8099/8099491.png" height={50} width={50}/> 
                 <span>Hi</span>
             </div>
-            <div className={style.nav_items}>
+            <div className={styles.nav_items}>
                 {items.map((item) => <span>{item}</span>)}
             </div>
             <Button text="Download Resume"/>
-
         </div>
         </>
     )
